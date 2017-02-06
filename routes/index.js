313bd494f7b91749584e1b49ae3b9af62d74c024
jack_carter <j.kuliga.kontakt@gmail.com>
2017-02-06 12:36:30 +0000

    fix

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var moment = require('moment');
 var router = express.Router();
 
 /* GET home page. */
@@ -9,13 +10,14 @@ router.get('/', function(req, res, next) {
   if ((minutes % 2)==0)
     idx = (minutes>20)?0:1;
   else idx = (minutes>20)?2:3;
+  let date2 = moment(new Date()).format();
   const status = [
     'Duże','Średnie','Małe','Brak'
   ];
-
   res.json({
     'title': 'Elektrownia',
-    'request': status[idx]
+    'request': status[idx],
+    'update_time': date2,
   });
 });
 
