91c32b1449ad8cb6b20dbd14051da7e12d8f2e3f
Jarosław Kuliga <j.kuliga.kontakt@gmail.com>
2017-02-16 07:20:41 +0100

    sync

--- app.js
+++ app.js
@@ -4,11 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
+var moment = require('moment');
 
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -21,7 +19,33 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+let elektrownia = {};
+
+setInterval(function() {
+console.log("STATUS")
+  const minutes = new Date().getMinutes();
+  let idx;
+  if ((minutes % 2)==0)
+    idx = (minutes>20)?0:1;
+  else idx = (minutes>20)?2:3;
+  let date2 = moment(new Date()).format();
+  const status = [
+    'Duże','Średnie','Małe','Brak'
+  ];
+  elektrownia = {
+    'title': 'Elektrownia',
+    'request': status[idx],
+    'update_time': date2,
+    'all_status': status
+  };
+}, 30000);
+
+
+app.use('/', function(req, res, next) {
+    res.json(elektrownia);
+});
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
